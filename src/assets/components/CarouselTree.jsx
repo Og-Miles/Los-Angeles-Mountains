dcb1d99651827f6aea8740d317f081ea3ff40455
EMMANUEL OGBUJI <95836608+Og-Miles@users.noreply.github.com>
2024-03-12 19:40:32 +0100

    Add files via upload

--- src/assets/components/CarouselTree.jsx
+++ src/assets/components/CarouselTree.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -39,24 +40,27 @@ export default function CarouselTree() {
   };
 
   return (
-    <div className='mx-auto min-h-full flex'>
-      <Slider
-        {...settings}
-        className='w-4/5 lg:w-2/3 xl:w-1/2 mx-auto flex justify-between gap-2'
-      >
-        <div>
-          <img src={carousel1} alt='mountain-man' className='max-w-full' />
-        </div>
-        <div>
-          <img src={carousel2} alt='mountain' className='max-w-full' />
-        </div>
-        <div>
-          <img src={carousel1} alt='mountain-man' className='max-w-full' />
-        </div>
-        <div>
-          <img src={carousel2} alt='mountain' className='max-w-full' />
-        </div>
-      </Slider>
+    <div className='mx-auto flex'>
+      {/* Add a wrapper div to apply custom styles */}
+      <div className='mx-auto flex justify-between gap-2'>
+        <Slider
+          {...settings}
+          className=' flex max-w-40 lg:max-w-5xl gap-2 justify-between'
+        >
+          <div>
+            <img src={carousel1} alt='mountain-man' />
+          </div>
+          <div>
+            <img src={carousel2} alt='mountain' />
+          </div>
+          <div>
+            <img src={carousel1} alt='mountain-man' />
+          </div>
+          <div>
+            <img src={carousel2} alt='mountain' />
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
